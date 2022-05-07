993dbca7d22c642167d9928435b8f218604332dc
iSky34 <103951064+iSky34@users.noreply.github.com>
2022-05-07 20:55:45 +0800

    Update train.py

--- train.py
+++ train.py
@@ -92,17 +92,13 @@ from albumentations.pytorch import ToTensorV2
 def get_transforms(train=False):
     if train:
         transform = A.Compose([
-            A.Resize(600, 600), # our input size can be 600px
-            A.HorizontalFlip(p=0.3),
-            A.VerticalFlip(p=0.3),
-            A.RandomBrightnessContrast(p=0.1),
-            #A.ColorJitter(p=0.1),
-            ToTensorV2()
+           
+        torchvision.transforms.ToTensor()
         ], bbox_params=A.BboxParams(format='coco'))
     else:
         transform = A.Compose([
-            A.Resize(600, 600), # our input size can be 600px
-            ToTensorV2()
+            
+        torchvision.transforms.ToTensor()
         ], bbox_params=A.BboxParams(format='coco'))
     return transform
 
